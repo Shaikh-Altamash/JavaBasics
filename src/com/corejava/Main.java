3981f8d69f9a1f9327e89165920dc92802ca9dc5
ALtamash <alizeh1157@gmail.com>
2021-12-06 11:04:16 +0530

    daily wage for one day calculated

--- src/com/corejava/Main.java
+++ src/com/corejava/Main.java
@@ -3,27 +3,40 @@ package com.corejava;
 public class Main {
 
      public static void main(String[] args) {
-     System.out.println("Welcome to employee base computation program ");
-     int wagePerHour = 20;
-     int fullDayHour = 8;
-     int halfDayHour = 4;
-     double checkEmployee =  Math.floor((Math.random() * 10) % 3);
-     switch ((int) checkEmployee)
-     {
-          case 0:
-               System.out.println("Employee is present full day");
-               System.out.println("Employee wage is " + wagePerHour*fullDayHour );
-          break;
+          System.out.println("Welcome to employee base computation program ");
+          int wagePerHour = 20;
+          int fullDayHour = 8;
+          int halfDayHour = 4;
+          int totalWorkingDayInMonth = 20;
+          int workingDays = 0;
+          int monthlySalary = 0;
+          while(workingDays < totalWorkingDayInMonth)
+          {
+               double checkEmployee =  Math.floor((Math.random() * 10) % 3);
+               switch ((int) checkEmployee)
+               {
+                    case 0:
+                         System.out.println("Employee is present full day");
+                         System.out.println("Employee wage is " + wagePerHour*fullDayHour );
+                         workingDays ++;
+                         monthlySalary += wagePerHour*fullDayHour;
+                         break;
 
-          case 1:
-               System.out.println("Employee is present half day");
-               System.out.println("Employee wage is " + wagePerHour*halfDayHour);
-          break;
-          case 2:
-               System.out.println("employee is absent");
-               break;
-          default:
-               System.out.println("exception caught");
-     }
+                    case 1:
+                         System.out.println("Employee is present half day");
+                         System.out.println("Employee wage is " + wagePerHour*halfDayHour);
+                         workingDays ++;
+                         monthlySalary += wagePerHour*halfDayHour;
+                         break;
+                    case 2:
+                         System.out.println("employee is absent");
+                         break;
+                    default:
+                         System.out.println("exception caught");
+               }
+          }
+
+          System.out.println("monthly salary is" + monthlySalary);
+
      }
 }
