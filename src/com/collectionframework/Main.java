6586913775132886c6e1fd4c787d5a916e2962e6
ALtamash <alizeh1157@gmail.com>
2021-12-06 10:28:07 +0530

    Empolyee presenty

--- src/com/collectionframework/Main.java
+++ src/com/collectionframework/Main.java
@@ -2,7 +2,17 @@ package com.collectionframework;
 
 public class Main {
 
-    public static void main(String[] args) {
+     public static void main(String[] args) {
      System.out.println("Welcome to employee base computation program ");
-    }
+
+     double checkEmployee = Math.floor((Math.random() * 10) % 2);
+     if(checkEmployee == 0)
+     {
+         System.out.println("employee is present");
+     }
+     else
+     {
+         System.out.println("employee is absent");
+     }
+     }
 }
