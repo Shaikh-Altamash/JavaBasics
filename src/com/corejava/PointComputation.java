738de81e0232d0f50d5e1de4560bf2eeae2fcb01
ALtamash <alizeh1157@gmail.com>
2021-12-13 12:11:13 +0530

    Program for Compare two Lengths of Line

--- src/com/corejava/PointComputation.java
+++ src/com/corejava/PointComputation.java
@@ -9,6 +9,5 @@ public class PointComputation {
         double length = Math.sqrt(Math.pow(p2.x - p1.x,2) + Math.pow(p2.y - p1.y,2));
         System.out.println(p1.distance(p2));
         System.out.println("Line of length is " + length);
-
     }
 }
