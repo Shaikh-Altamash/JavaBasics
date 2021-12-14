118a050be0b0062fc46018b3e995bb32be83dc7a
ALtamash <alizeh1157@gmail.com>
2021-12-14 21:05:17 +0530

    Programme for CRUD operations on arraylist

--- src/com/corejava/AddressBookHandler.java
+++ src/com/corejava/AddressBookHandler.java
@@ -34,8 +34,49 @@ public class AddressBookHandler {
 
                     break;
                 case 2:
+                    boolean isUpdated = false;
+                    System.out.print("Enter user first name to update address book : ");
+                    String userNameToUpdate = scanner.next();
+                    for(int i = 0; i<addressBookList.size();i++){
+                        if(addressBookList.get(i).firstName.equalsIgnoreCase(userNameToUpdate)){
+                            System.out.println("Enter your first name  : ");
+                            addressBookList.get(i).firstName = scanner.next();
+                            System.out.println("Enter your last name  : ");
+                            addressBookList.get(i).lastName = scanner.next();
+                            System.out.println("Enter your address  : ");
+                            addressBookList.get(i).address = scanner.next();
+                            System.out.println("Enter your village : ");
+                            addressBookList.get(i).village = scanner.next();
+                            System.out.println("Enter your city  : ");
+                            addressBookList.get(i).city = scanner.next();
+                            System.out.println("Enter your state : ");
+                            addressBookList.get(i).state = scanner.next();
+                            System.out.println("Enter your pin code : ");
+                            addressBookList.get(i).pinCode = scanner.nextInt();
+                            System.out.println("Contact updated successfully ");
+                            isUpdated = true;
+                        }
+                    }
+                    if(!isUpdated){
+                        System.out.println("No user found of given name ");
+                    }
                     break;
                 case 3:
+                    System.out.print("Enter user name to delete contact : ");
+                    String userNameToDelete = scanner.next();
+                    AddressBook remove = null;
+                    for(int i =0; i < addressBookList.size() ;i++){
+                        if(addressBookList.get(i).firstName.equalsIgnoreCase(userNameToDelete)){
+                          remove = addressBookList.remove(i);
+                        }
+                    }
+                    if(remove != null){
+                        System.out.println("Contact deleted successfully ");
+                    }
+                    else{
+
+                        System.out.println("No user found of given name ");
+                    }
                     break;
                 case 4:
                     System.out.println("The contacts you have in your address book");
